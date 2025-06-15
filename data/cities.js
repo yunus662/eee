c147a3d824949451868907b8a7fea2c6c62cbc33
yunus662 <imyaay234@gmail.com>
2025-06-14 23:34:42 -0700

    Delete data directory

--- data/cities.js
+++
@@ -1,9 +0,0 @@
-[
-  { "name": "Washington, D.C.", "lat": 38.895, "lon": -77.0366, "population": 705749, "country": "United States", "type": "capital" },
-  { "name": "London", "lat": 51.5074, "lon": -0.1278, "population": 8900000, "country": "United Kingdom", "type": "capital" },
-  { "name": "Berlin", "lat": 52.52, "lon": 13.405, "population": 3769000, "country": "Germany", "type": "capital" },
-  { "name": "Tokyo", "lat": 35.6895, "lon": 139.6917, "population": 13960000, "country": "Japan", "type": "capital" },
-  { "name": "Cairo", "lat": 30.0444, "lon": 31.2357, "population": 9900000, "country": "Egypt", "type": "capital" },
-  { "name": "Mumbai", "lat": 19.076, "lon": 72.8777, "population": 12440000, "country": "India", "type": "major" },
-  { "name": "São Paulo", "lat": -23.5505, "lon": -46.6333, "population": 12300000, "country": "Brazil", "type": "major" }
-]
