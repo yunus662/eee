f7537be3b7a73439445cb20d3f5ac1f8ce376896
yunus662 <imyaay234@gmail.com>
2025-06-15 15:37:52 -0700

    Update economy.js

--- economy.js
+++ economy.js
@@ -5,5 +5,19 @@ export function generateMarketOffers() {
     good,
     price: Math.floor(Math.random() * 100 + 50),
     quantity: Math.floor(Math.random() * 1000 + 100)
+    export const economy = {
+    
+  money: 1000,
+  food: 500,
+  materials: 300,
+  population: 100
+};
+
+export function produceResources() {
+  economy.food += 5;
+  economy.materials += 3;
+  economy.money += 10;
+}
+
   }));
 }
