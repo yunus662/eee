6f7d920d3b4efb4c57d101109b55cfa6ab42ab0d
yunus662 <imyaay234@gmail.com>
2025-06-14 23:34:53 -0700

    Delete scripts directory

--- scripts/combat.js
+++
@@ -1,9 +0,0 @@
-export function resolveCombat(attacker, defender) {
-  const baseDamage = attacker.damage;
-  const defense = defender.defense;
-  const net = baseDamage - (defense * 0.7) + Math.random() * 3;
-
-  if (net > defense) return "attackerWins";
-  if (net > defense / 2) return "stalemate";
-  return "defenderHolds";
-}
--- scripts/economy.js
+++
@@ -1,9 +0,0 @@
-export const TradeGoods = ["Fuel", "Supplies", "Rare Metals", "Consumer Goods"];
-
-export function generateMarketOffers() {
-  return TradeGoods.map(good => ({
-    good,
-    price: Math.floor(Math.random() * 100 + 50),
-    quantity: Math.floor(Math.random() * 1000 + 100)
-  }));
-}
