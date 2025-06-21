7ac1d1ac963d63664bf113b0b55a40c04fd08e34
yunus662 <imyaay234@gmail.com>
2025-06-20 19:25:13 -0700

    Delete scripts directory

--- scripts/survey.js
+++
@@ -1,7 +0,0 @@
-export function discoverResources(region) {
-  const findings = [];
-  if (Math.random() > 0.8) findings.push("Oil");
-  if (Math.random() > 0.85) findings.push("Uranium");
-  if (Math.random() > 0.9) findings.push("Rare Earths");
-  return findings;
-}
